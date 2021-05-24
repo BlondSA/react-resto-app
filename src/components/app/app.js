339b58b1bce208966a2674d273cec5dcaf75771a
St.Anton <striuchkov.anton@gmail.com>
2021-05-24 17:55:49 +0300

    Add json-server

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import {MainPage, CartPage} from '../pages';
-import AppHeader from '../app-header';
+import React from "react";
+import { MainPage, CartPage } from "../pages";
+import AppHeader from "../app-header";
+import WithRestoService from "../hoc/";
+import { Switch, Route } from "react-router-dom";
 
-import Background from './food-bg.jpg';
+import Background from "./food-bg.jpg";
 
-const App = () => {
-    return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
-        </div>
-    )
-}
+const App = ({ RestoService }) => {
+  RestoService.getMenuItems().then((menu) => console.log(menu)).catch((error) => console.log(error));
+  return (
+    <div
+      style={{ background: `url(${Background}) center center/cover no-repeat` }}
+      className="app"
+    >
+      <AppHeader total={50} />
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/cart" component={CartPage} />
+      </Switch>
+      <MainPage />
+    </div>
+  );
+};
 
-export default App;
+export default WithRestoService()(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import ErrorBoundary from "./components/error-boundary";
+import RestoServiceContext from "./components/resto-service-context";
+import RestoService from "./services/resto-service";
+import store from "./store";
 
-import './index.scss';
+import "./index.scss";
 
+const restoService = new RestoService();
 
 ReactDOM.render(
-    <App/>
-    , document.getElementById('root'));
-
+  <Provider store={store}>
+    <ErrorBoundary>
+      <RestoServiceContext.Provider value={restoService}>
+        <Router>
+          <App />
+        </Router>
+      </RestoServiceContext.Provider>
+    </ErrorBoundary>
+  </Provider>,
+  document.getElementById("root")
+);
